7b7d44d77f84dac30659e368cd261f32de4ce16c
Kwendo-K <kevinkwendo95@gmail.com>
2022-02-14 00:03:32 +0300

    A small project on registration and login form

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a login and register system project.
+The system gets the user data and stores it in the database
+This project has been implemente using html, bootstrap and php
---
+++ login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body class="bg-primary">
+   <div class="container p-2">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-5">
+                        image here
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="text-center">
+                            <h4 class="grey">Create an Account</h4>
+                        </div>
+                        <form action="#">
+                            <div class="row mb-3">
+                            <input class="form-control rounded-pill" type="mail" name="email" placeholder="example@gmail.com">
+                            </div>
+                            <div class="row mb-3">
+                                    <input class="form-control rounded-pill" type="password" name="password" placeholder="password">
+                            </div>
+                            <div class="row mb-3">
+                                <input  class="rounded-pill btn btn-primary" type="submit" name="submit" value="Login">
+                            </div>
+                            <hr>
+                            <div class="row mb-3">
+                                <a href=""  class="rounded-pill btn btn-danger">
+                                    <i class="fa fa-google"></i>
+                                    Login with google
+                                </a>
+                            </div>
+                        </form>
+                        <hr>
+                        <div class="text-center">
+                            <a class="small" href="">Login</a>
+                        </div>
+                        <div class="text-center">
+                            <a class="small" href="">Forgot Password?</a>
+                        </div>
+                        <div class="text-center">
+                            <a class="small" href="register.php">Register Account</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+   </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script> 
+</body>
+</body>
+</html>
